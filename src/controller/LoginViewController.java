75d867e14e3e0ff118c3fb616ffd26d4f589a636
Tharindu527 <tharindudakshinadaraz@gmail.com>
2021-12-20 13:46:17 +0530

    update content

---
+++ src/controller/LoginViewController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginViewController {
+    public PasswordField psdPassword;
+    public TextField txtUSerName;
+
+    public void loginOnAction(ActionEvent event) {
+
+    }
+}
