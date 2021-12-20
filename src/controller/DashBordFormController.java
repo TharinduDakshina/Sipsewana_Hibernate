5f92230556bcf77d3658700073039d92d26e2af0
Tharindu527 <tharindudakshinadaraz@gmail.com>
2021-12-20 21:19:58 +0530

    update content

--- src/controller/DashBordFormController.java
+++ src/controller/DashBordFormController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import javafx.scene.input.MouseEvent;
+
 public class DashBordFormController {
 
+    public void registerClickOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void showDetailsClickOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void updateClickOnAction(MouseEvent mouseEvent) {
+
+    }
 }
