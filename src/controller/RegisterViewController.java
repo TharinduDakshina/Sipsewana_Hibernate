1ebd97693657a3bd236b77f50a5b4cd3fb756c8a
Tharindu527 <tharindudakshinadaraz@gmail.com>
2021-12-21 21:54:35 +0530

    update Content

--- src/controller/RegisterViewController.java
+++ src/controller/RegisterViewController.java
@@ -1,5 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
 public class RegisterViewController {
 
+    public AnchorPane registrationDashBord;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public Label lblStudentId;
+    public Label lblDate;
+    public RadioButton genderMale;
+    public ToggleGroup Gender;
+    public RadioButton genderFemale;
+    public RadioButton genderOther;
+    public JFXComboBox cmbCourses;
+    public TableView tblCourse;
+    public TableColumn colCourseName;
+
+    public void btnAddCourse(ActionEvent event) {
+    }
+
+    public void btnRemoveCouse(ActionEvent event) {
+    }
+
+    public void registerStudentOnAction(ActionEvent event) {
+    }
 }
